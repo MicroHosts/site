1f411e01eaaed3711ba92b9b5c0155348de04eb5
1Suro1 <timu.zaxarov@gmail.com>
2023-03-12 13:56:01 +0900

    edit

--- pages/api/auth/createUser.ts
+++ pages/api/auth/createUser.ts
@@ -9,32 +9,46 @@ export default async function handler(
         res.status(500).json("Укажите почту и пароль")
         return
     }
-    const formData = new FormData();
-    formData.append("adduser", "1");
-    formData.append("priority", "2");
-    formData.append("uplid", "1");
-    formData.append("newpass", password);
-    formData.append("newemail", email);
-    formData.append("inhouse_billing", "1");
-    formData.append("num_users", "2");
-    formData.append("space", "150");
-    formData.append("ram", "8192");
-    formData.append("burst", "1024");
-    formData.append("bandwidth", "200");
-    formData.append("cores", "4");
-    formData.append("space_per_vm", "20");
-    formData.append("cpu_percent", "40");
-    formData.append("num_ipv4", "5");
-    formData.append("txt_service_period", "1");
-    formData.append("allowed_virts[]", "kvm");
+    // const formData = new FormData();
+    // formData.append("adduser", "1");
+    // formData.append("priority", "2");
+    // formData.append("uplid", "1");
+    // formData.append("newpass", password);
+    // formData.append("newemail", email);
+    // formData.append("inhouse_billing", "1");
+    // formData.append("num_users", "2");
+    // formData.append("space", "150");
+    // formData.append("ram", "8192");
+    // formData.append("burst", "1024");
+    // formData.append("bandwidth", "200");
+    // formData.append("cores", "4");
+    // formData.append("space_per_vm", "20");
+    // formData.append("cpu_percent", "40");
+    // formData.append("num_ipv4", "5");
+    // formData.append("txt_service_period", "1");
+    // formData.append("allowed_virts[]", "kvm");
+    var params = new URLSearchParams();
+    params.append("adduser", "1");
+    params.append("priority", "2");
+    params.append("uplid", "1");
+    params.append("newpass", password);
+    params.append("newemail", email);
+    params.append("inhouse_billing", "1");
+    params.append("num_users", "2");
+    params.append("space", "150");
+    params.append("ram", "8192");
+    params.append("burst", "1024");
+    params.append("bandwidth", "200");
+    params.append("cores", "4");
+    params.append("space_per_vm", "20");
+    params.append("cpu_percent", "40");
+    params.append("num_ipv4", "5");
+    params.append("txt_service_period", "1");
+    params.append("allowed_virts[]", "kvm");
     const response = await fetch(`https://churkahost.float-zone.com:4085/index.php?act=adduser&api=json&adminapikey=${process.env.API_KEY}&adminapipass=${process.env.KEY_PASS}`,
         {
         method: "POST",
-        //     headers:{
-        //         'Content-Type': 'application/x-www-form-urlencoded'
-        //     },
-            // @ts-ignore
-        body: formData
+        body: params
         }
         )
     const json = await response.json();
